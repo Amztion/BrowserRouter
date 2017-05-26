a06db130caabaa7f84614a82dc1252c3ed7bfaf9
zhaoliang06 <zhaoliang06@meituan.com>
2017-05-26 12:50:38 +0800

    Initial Commit

---
+++ BrowserRouter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BrowserRouter
+//
+//  Created by Liang Zhao on 2017/5/26.
+//  Copyright © 2017年 Liang Zhao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BrowserRouter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BrowserRouter
+//
+//  Created by Liang Zhao on 2017/5/26.
+//  Copyright © 2017年 Liang Zhao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
