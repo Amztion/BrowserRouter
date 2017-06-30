666eb90f9a872be6abf52d082b97d38f9496a9af
zhaoliang06 <zhaoliang06@meituan.com>
2017-06-30 18:45:27 +0800

    [+]Add Regex and WildCard Class

---
+++ BrowserRouter/Regex.swift
@@ -0,0 +1,27 @@
+//
+//  Regex.swift
+//  BrowserRouter
+//
+//  Created by Liang Zhao on 2017/6/30.
+//  Copyright © 2017年 Liang Zhao. All rights reserved.
+//
+
+import Cocoa
+
+class Regex {
+    private let regularExpression: NSRegularExpression
+    
+    init?(pattern: String) {
+        
+        if let `regularExpression` = try? NSRegularExpression(pattern: pattern, options: .anchorsMatchLines) {
+            self.regularExpression = regularExpression
+        } else {
+            return nil
+        }
+    }
+    
+    func match(pattern: String) -> Bool {
+        return self.regularExpression.matches(in: pattern, options: .anchored, range: NSRange(location: 0, length: pattern.characters.count)).count > 0
+    }
+}
+
---
+++ BrowserRouter/WildCard.swift
@@ -0,0 +1,42 @@
+//
+//  WildCard.swift
+//  BrowserRouter
+//
+//  Created by Liang Zhao on 2017/6/30.
+//  Copyright © 2017年 Liang Zhao. All rights reserved.
+//
+
+import Cocoa
+
+class WildCardURL {
+    private let url: String
+    let regex: Regex
+    let charactersMap = [
+        "/": "\\/",
+        ".": "\\.",
+        "?": "\\?"
+    ]
+    
+    init?(url: String) {
+        print(url)
+        
+        var replacedUrl = url
+        
+        for key in self.charactersMap.keys {
+            if let replacedChar = self.charactersMap[key] {
+                replacedUrl = replacedUrl.replacingOccurrences(of: key, with: replacedChar)
+            }
+        }
+        
+        replacedUrl = replacedUrl.replacingOccurrences(of: "*", with: "[^ ]*")
+        
+        print(replacedUrl)
+        self.url = replacedUrl
+        
+        if let rege = Regex(pattern: replacedUrl) {
+            self.regex = rege
+        } else {
+            return nil
+        }
+    }
+}
